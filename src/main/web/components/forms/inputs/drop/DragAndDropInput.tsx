7ab12c9a7e4cae65ff45de330b6273e1edd92773
Cristina Giancristofaro <CGiancristofaro@britishmuseum.org>
2020-10-01 17:15:03 +0200

    Drag and drop input: modified card dimensions and style
    Signed-off-by: Cristina Giancristofaro <CGiancristofaro@britishmuseum.org>

--- src/main/web/components/forms/inputs/drop/DragAndDropInput.tsx
+++ src/main/web/components/forms/inputs/drop/DragAndDropInput.tsx
@@ -114,7 +114,7 @@ export class DragAndDropInput extends MultipleValuesInput<DragAndDropInputProps,
       </div>
     `,
     itemTemplate: `
-        {{#> rsp:itemCardTemplate width=135 height=175 cardMargin=8 footer-paddingY=7 footer-paddingX=8}}
+        {{#> rsp:itemCardTemplate width=105 height=125 cardMargin=4 footer-paddingY=7 footer-paddingX=8}}
           {{#*inline "additionalActions"}}
             <div class="rs-default-card__hover-icon">
               <mp-event-trigger type='Form.DragAndDropInput.RemoveItem' targets='["{{inputId}}"]' data='{"iri": "{{iri}}"}'>

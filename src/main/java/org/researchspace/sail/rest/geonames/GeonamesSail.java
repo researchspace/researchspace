ede095d8d85fb33df11efda9e6420d8e9a2140c6
gspinaci <gianmarco.spinaci@gmail.com>
2021-01-27 16:34:00 +0100

    Starting Geonames federation
    Signed-off-by: gspinaci <gianmarco.spinaci@gmail.com>

---
+++ src/main/java/org/researchspace/sail/rest/geonames/GeonamesSail.java
@@ -0,0 +1,46 @@
+/**
+ * ResearchSpace
+ * Copyright (C) 2020, © Trustees of the British Museum
+ * Copyright (C) 2015-2019, metaphacts GmbH
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.researchspace.sail.rest.geonames;
+
+import org.eclipse.rdf4j.sail.SailConnection;
+import org.eclipse.rdf4j.sail.SailException;
+import org.researchspace.sail.rest.AbstractServiceWrappingSail;
+
+/**
+ * 
+ * @author Janmaruko Hōrensō <@gspinaci>
+ *
+ */
+
+
+public class GeonamesSail extends AbstractServiceWrappingSail {
+
+
+
+    public GeonamesSail(String url) {
+        super(url);
+    }
+
+    @Override
+    protected SailConnection getConnectionInternal() throws SailException {
+        return null;
+    }
+    
+}

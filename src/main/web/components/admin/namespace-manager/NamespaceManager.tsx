58588a7b28ecf2d7ba88e1c003f331dd87ee7c7d
Cristina Giancristofaro <CGiancristofaro@britishmuseum.org>
2021-06-22 15:08:23 +0100

    ui changes for inputs and buttons
    Signed-off-by: Cristina Giancristofaro <CGiancristofaro@britishmuseum.org>

--- src/main/web/components/admin/namespace-manager/NamespaceManager.tsx
+++ src/main/web/components/admin/namespace-manager/NamespaceManager.tsx
@@ -264,10 +264,10 @@ function createActionsCellRenderer(params: { onDelete: (record: PrefixRecord) =>
           <div>
             Delete prefix "{record.prefix}"?
             <div>
-              <Button className="btn-grey-dark-border btn-danger" onClick={this.onConfirm}>
+              <Button className="btn-grey" onClick={this.onConfirm}>
                 Delete
               </Button>
-              <Button className="btn-grey-dark-border" onClick={this.onCancel}>
+              <Button className="btn-grey" onClick={this.onCancel}>
                 Cancel
               </Button>
             </div>
@@ -275,8 +275,8 @@ function createActionsCellRenderer(params: { onDelete: (record: PrefixRecord) =>
         );
       } else {
         return (
-          <Button className="btn-grey-dark-border btn-danger" onClick={this.onDeleteClick}>
-            <span className="fa fa-trash-o" />
+          <Button className="btn-grey" onClick={this.onDeleteClick}>
+            <span className="fa fa-trash-o btn-icon-left" />
             &nbsp;Delete
           </Button>
         );

3806f9609c58db22410ae4f409dd4ea2fc5dc0c6
DawsonMacPhee <dawsonmacphee@icloud.com>
2022-04-14 15:19:32 -0400

    Refactored to react
    Signed-off-by: DawsonMacPhee <dawsonmacphee@icloud.com>

--- src/main/web/components/timeline/HybridTimeline.tsx
+++ src/main/web/components/timeline/HybridTimeline.tsx
@@ -1,225 +1,165 @@
-var vis = require("vis-timeline");
-var data = require("vis-data");
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import * as _ from 'lodash';
+import { Component, ComponentProps, ComponentContext} from 'platform/api/components';
+import { SparqlClient } from 'platform/api/sparql';
+import { DataSet, Timeline } from 'vis';
+import { trigger } from 'platform/api/events';
+import { addToDefaultSet } from 'platform/api/services/ldp-set';
+import { Rdf } from 'platform/api/rdf';
+import * as uuid from 'uuid';
 
-class lincsTimelineHoriz1 extends HTMLElement {
-    constructor() {
-        super();
-        this.innerHTML = "";
-        var template = 
-        `
-        <div style="display: flex; flex-direction: row;">
-            <div id="lincs-timeline" style="width: 66%; margin: 2%; border: 1px solid lightgray; flex-grow: 4;"></div>
-            <div style="background-color: lightgrey; width: 30%; margin-top: 2%; height: 500px;">
-                <div id="sidebarContent" style="height: 500px; overflow-y: auto;">
-                    <div style="display: flex; flex-direction: row; align-items: center; margin-left: 20px;">
-                        <h3 id="sidebarTitle">Vertical Timeline</h3>
-                    </div>
-                    <style id="vrstyleBlock">.vrtimeline::before {height: 0%;}</style>
-                    <div class="vrtimeline" style="max-width: 400px; min-width: 200px;">
-                        <ul id="vrlist" style="margin: 0px; width: 100%;">
-                        
-                        </ul>
+export interface HybridTimelineConfig {
+    query: string;
+}
+export type HybridTimelineProps = HybridTimelineConfig & React.Props<HybridTimeline> & ComponentProps;
+
+interface HybridTimelineState {
+    items: DataSet<any>;
+    yearSidebar: Array<Array<any>>;
+    parsedDates: Array<any>;
+    timeline: any;
+}
+
+export class HybridTimeline extends Component<HybridTimelineProps, HybridTimelineState> {
+    context: ComponentContext;
+    timelineRef = React.createRef<HTMLDivElement>();
+    styleId = uuid.v1();
+    constructor(props: HybridTimelineConfig, context: ComponentContext) {
+        super(props, context);
+        this.state = {items: new DataSet([]), yearSidebar: [], parsedDates: [], timeline: null};
+
+    }
+
+    public componentDidMount() {
+        let colours = ["rgb(255, 138, 138)", "rgb(112, 215, 255)", "rgb(205, 135, 255)", "rgb(102, 217, 138)", "rgb(69, 131, 255)", "rgb(255, 174, 69)", "rgb(245, 223, 83)", "rgb(255, 110, 194)"];
+
+        SparqlClient.select(this.props.query).onValue(
+            (response) => {
+                let years = [];
+                let types = [];
+                for (const date of response.results.bindings) {
+                    let year = date.start.value.split("-")[0];
+                    if (!years.includes(year)) {
+                        this.state.parsedDates[year] = [];
+                        years.push(year);
+                    }
+
+                    if (!types.includes(date.type.value)) {
+                        types.push(date.type.value);
+                    }
+
+                    let colour = "";
+                    if (types.indexOf(date.type.value) > (colours.length - 1)) {
+                        colour = colours[colours.length - 1];
+                    } else {
+                        colour = colours[types.indexOf(date.type.value)];
+                    }
+
+                    this.state.parsedDates[year].push({title: date.label.value, date: date.start.value, iri: date.subject.value, colour: colour});
+                }
+
+                for (let i = 0; i < years.length; i++) {
+                    let year = years[i];
+
+                    let dateCard = document.createElement('div');
+                    let dateCardInternal = (
+                        <div style={{display: "flex", flexDirection: "row", alignItems: "center"}}>
+                            <h4 className="yearInfo"> { year } </h4>
+                            <img onClick={() => {this.openVerticalTimeline(year)}} className="stackButton" src="/assets/components/timeline/imgs/stack.png" style={{cursor: "pointer", height: "40px", width: "49px", padding: "8px"}} />
+                            <span style={{position: "absolute", right: "16.5px", top: "22px", pointerEvents: "none"}}>{ this.state.parsedDates[year].length }</span>
+                        </div>
+                    );
+                    ReactDOM.render(dateCardInternal, dateCard);
+
+                    this.state.yearSidebar[year] = [];
+                    for (let y = 0; y < this.state.parsedDates[year].length; y++) {
+                        this.state.yearSidebar[year].push(
+                            <li>
+                                <div className="content" style={{ position: "relative", backgroundColor: this.state.parsedDates[year][y].colour }}>
+                                    <h3>{this.state.parsedDates[year][y].date.split("-")[0]}: {this.state.parsedDates[year][y].title}</h3>
+                                    <div className="toolIcons">
+                                        <a onClick={() => {trigger({eventType: 'Dashboard.AddFrame',  targets: ["thinking-frames"], source: 'timeline-trigger', data: {"resourceIri": this.state.parsedDates[year][y].iri, "viewId": "resource"}})}}>
+                                            <i style={{color: "black", fontSize: "25px", padding: "0px 8px"}} className="rs-icon rs-icon-page"></i>
+                                        </a>
+                                        <a onClick={() => {trigger({eventType: 'Dashboard.AddFrame',  targets: ["thinking-frames"], source: 'timeline-trigger', data: {"resourceIri": this.state.parsedDates[year][y].iri, "viewId": "knowledge-map"}})}}>
+                                            <i style={{color: "black", fontSize: "25px", padding: "0px 8px"}} className="rs-icon rs-icon-diagram"></i>
+                                        </a>
+                                        <a onClick={() => {this.openResourceDetails(this.state.parsedDates[year][y].iri)}}>
+                                            <i style={{color: "black", fontSize: "25px", padding: "0px 8px"}} className="rs-icon rs-icon-sidebar_right"></i>
+                                        </a>
+                                        <a onClick={() => {this.addResourceToClipboard(this.state.parsedDates[year][y].iri)}}>
+                                            <img style={{height: "30px", padding: "0px 8px", paddingBottom: "2px"}} src="/assets/components/timeline/imgs/clipboard.png" />
+                                        </a>
+                                    </div>
+                                </div>
+                                <div className="vrpoint" style={{backgroundColor: (this.state.parsedDates[year][y].colour as string)}}></div>
+                            </li>
+                        );
+                    }
+
+                    this.state.items.add({id: i, content: dateCard, start: year + "-01-01"});
+                }
+
+                this.createTimeline();
+            }
+        );
+    }
+
+    public render() {
+        return(
+            <div style={{display: "flex", flexDirection: "row"}} ref={this.timelineRef}>
+                <div id="lincs-timeline" style={{width: "66%", margin: "2%", marginBottom: "0px", border: "1px solid lightgray", flexGrow: 4}}></div>
+                <div style={{backgroundColor: "lightgrey", width: "30%", marginTop: "2%", height: "500px"}}>
+                    <div id="sidebarContent" style={{height: "500px", overflowY: "auto"}}>
+                        <div style={{display: "flex", flexDirection: "row", alignItems: "center", marginLeft: "20px"}}>
+                            <h3 id="sidebarTitle">Vertical Timeline</h3>
+                        </div>
+                        <style id="vrstyleBlock">{"." + this.styleId.split("-")[0] + "::before {height: 0%;}"}</style>
+                        <div id="vrtimeline" className={this.styleId.split("-")[0]} style={{maxWidth: "400px", minWidth: "200px"}}>
+                            
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-        <div class="slidecontainer" id="slidecontainer" style='display: none;'>
-            <span id="hover-info-1"></span>
-            <span id="hover-info-2"></span>
-            <input type="range" id="range" min="1" max="100" value="1">
-        </div>
-        <bs-alert id="warning" bs-style="warning" class="search-results__alert" style="text-align: left; display: none;">
-            <i class="fa fa-exclamation-triangle" aria-hidden="true" style="margin-right: 10px;"></i>
-            <span class="num-results">The timeline can only display 150 dates at a time. Use the slider to move the date range.</span>
-        </bs-alert>
-        <div id="loading-block" class="modal-backdrop in" style="display: none;">
-            <span class="system-spinner">
-                <i class="system-spinner__icon"></i>
-                <span class="system-spinner__message">Please wait...</span>
-            </span>
-        </div>
-        `;
-        this.innerHTML = template;
-
-        this.loadingBlock = this.querySelector("#loading-block");
-        this.container = this.querySelector("#lincs-timeline");
-        this.listContainer = this.querySelector("#vrlist");
-        this.sidebarContainer = this.querySelector("#sidebarContent");
-        this.sidebarTitle = this.querySelector("#sidebarTitle");
-        this.styleBlock = this.querySelector("#vrstyleBlock");
-        this.slider = this.querySelector("#slidecontainer #range");
-        this.sliderContainer = this.querySelector("#slidecontainer");
-        this.hoverInfo1 = this.querySelector("#hover-info-1");
-        this.hoverInfo2 = this.querySelector("#hover-info-2");
-        this.warning = this.querySelector("#warning");
-        this.items = new data.DataSet([ ]);
-        this.entries = [];
-        this.yearDisplay = [];
-        this.years = [];
-        this.displayedYears = [];
+        );
     }
-    connectedCallback() {
-        var _this = this;
 
-        var types = [];
-        var colours = ["background-color: rgb(255, 138, 138);", "background-color: rgb(112, 215, 255);", "background-color: rgb(205, 135, 255);", "background-color: rgb(102, 217, 138);", "background-color: rgb(69, 131, 255);", "background-color: rgb(255, 174, 69);", "background-color: rgb(245, 223, 83);", "background-color: rgb(255, 110, 194);"];
-        var info = document.getElementById("qinfo");
-
-        var entries = info.textContent.split(";");
-        for (var i = 0; i < (entries.length / 4) - 1; i++) {
-            var entry = [];
-            entry[0] = entries[i * 4];
-            entry[1] = entries[(i * 4) + 1];
-            entry[2] = entries[(i * 4) + 2];
-            entry[4] = entries[(i * 4) + 3];
-
-            var ind = types.indexOf(entry[2]);
-            if (ind == -1) {
-                types.push(entry[2]);
-                ind = types.length - 1;
-            }
-
-            if (colours[ind] == null) {
-                entry[2] = "background-color: gray;";
-            } else {
-                entry[2] = colours[ind];
-            }
-
-            if (this.years.includes(entry[1].split("-")[0])) {
-                this.entries[entry[1].split("-")[0]].push(entry);
-            } else {
-                this.entries[entry[1].split("-")[0]] = [];
-                this.entries[entry[1].split("-")[0]].push(entry);
-
-                this.years.push(entry[1].split("-")[0]);
-            }
-        }
-
-        for (var i = 0; i < this.years.length; i++) {
-            var year = this.years[i];
-            var html = "";
-
-            var dateCard = document.createElement("div");
-            dateCard.innerHTML = `
-            <span hidden>" + year + "</span>
-            <div style="display: flex; flex-direction: row; align-items: center;">
-                <h4>` + year + `</h4>
-                <img src="/assets/components/timeline/imgs/stack.png" style="cursor: pointer; height: 40px; width: 49px; padding: 8px;" />
-                <span style="position: absolute; right: 16.5px; top: 22px; pointer-events: none;">` + this.entries[year].length + `</span>
-            </div>`;
-            var stackButton = dateCard.firstChild.nextSibling.nextSibling.nextSibling.firstChild.nextSibling.nextSibling.nextSibling;
-
-            for (var y = 0; y < this.entries[year].length; y++) {
-                html += `<li>
-                    <div class="content" style="position: relative; ` + this.entries[year][y][2] + `">
-                        <h3>` + this.entries[year][y][1].split("T")[0] + ": " + this.entries[year][y][0] + `</h3>
-                        <div class="toolIcons">
-                            <a href="/resource/?uri=` + this.entries[year][y][3] + `">
-                                <i style="color: black; font-size: 25px; padding: 0px 8px;" class="rs-icon rs-icon-page"></i>
-                            </a>
-                            <a href="/resource/:ThinkingFrames?view=knowledge-map&resource=` + this.entries[year][y][3] + `">
-                                <i style="color: black; font-size: 25px; padding: 0px 8px;" class="rs-icon rs-icon-diagram"></i>
-                            </a>
-                            <a onclick="openResourceDetails('` + this.entries[year][y][3] + `')">
-                                <i style="color: black; font-size: 25px; padding: 0px 8px;" class="rs-icon rs-icon-sidebar_right"></i>
-                            </a>
-                            <a onclick="addResourceToClipboard('` + this.entries[year][y][3] + `')">
-                                <img style="height: 30px; padding: 0px 8px; padding-bottom: 2px;" src="/assets/components/timeline/imgs/clipboard.png" />
-                            </a>
-                        </div>
-                    </div>
-                    <div class="vrpoint" style="` + this.entries[year][y][2] + `"></div>
-                </li>`
-            }
-            
-            stackButton.addEventListener("click", function() {
-                _this.openVerticalTimeline(this);
-            });
-
-            this.yearDisplay[year] = [];
-            this.yearDisplay[year][0] = dateCard;
-            this.yearDisplay[year][1] = html;
-            if (i < 150 && !this.displayedYears.includes(year)) {
-                this.items.add({id: this.displayedYears.length, content: this.yearDisplay[year][0], start: year + "-01-01"});
-                this.displayedYears.push(year);
-            }
-        }
-        this.createTimeline();
+    private openVerticalTimeline(year) { // This function opens the sidebar timeline
+        let listContainer = this.timelineRef.current.querySelector("#vrtimeline");
+        ReactDOM.unmountComponentAtNode(listContainer);
+        let renderComponent = (
+            <ul id="vrlist" style={{margin: "0px", width: "100%"}}>
+                {this.state.yearSidebar[year]}
+            </ul>
+        );
+        ReactDOM.render(renderComponent, listContainer);
+        this.timelineRef.current.querySelector("#vrstyleBlock").innerHTML = "." + this.styleId.split("-")[0] + "::before {height: " + this.timelineRef.current.querySelector("#vrlist").scrollHeight + "px;}";
     }
-    openVerticalTimeline(element) { // This function opens the sidebar timeline
-        this.sidebarTitle.textContent = "Vertical Timeline";
-        this.listContainer.innerHTML = this.yearDisplay[element.previousSibling.previousSibling.textContent][1];
-        this.listContainer.style.visibility = "visible";
 
-        this.styleBlock.innerHTML = ".vrtimeline::before {height: " + this.listContainer.scrollHeight + "px;}";
+    private openResourceDetails(iri) {
+        trigger({
+            eventType: 'Dashboard.OpenSidebar',  
+            targets: ["thinking-frames"], 
+            source: 'timeline-trigger',
+            data: {"iri":  iri}
+        });
     }
-    createTimeline() { // This function creates the timeline
-        var _this = this;
-        var options = {
+
+    private addResourceToClipboard(iri) {
+        addToDefaultSet(Rdf.iri(iri), 'timeline-trigger').onValue(() => {
+            trigger({eventType: 'Component.Refresh', targets: ["clipboard"], source: 'timeline-trigger'});
+        });
+    }
+
+    private createTimeline() { // This function creates the timeline
+        let options = {
             height: '500px',
             zoomMin: 378683420000
         };
             
-        this.timeline = new vis.Timeline(this.container, this.items, options);
-
-        if (this.displayedYears.length > 150) {
-            this.sliderContainer.style.display = "block";
-            this.warning.style.display = "block";
-
-            this.hoverInfo1.textContent = this.displayedYears[0];
-            this.hoverInfo2.textContent = this.displayedYears[150];
-            this.hoverInfo1.style.left = (-this.hoverInfo1.offsetWidth) + "px";
-            this.hoverInfo2.style.left = 65 + "px";
-
-            this.slider.addEventListener("mouseup", function () {
-                _this.updateDates(this.value);
-            });
-
-            this.slider.addEventListener("input", function() {
-                _this.updateSlider(this.value);
-            });
-        }
+        let timeline = new Timeline(this.timelineRef.current.querySelector("#lincs-timeline"), this.state.items, options);
     }
-    updateDates(value) { // This function updates the slider dates
-        var _this = this;
-        this.loadingBlock.style.display = "block";
-        
-        var start;
-        if (value == 1) {
-            start = 0;
-        } else {
-            start = Math.floor((value / 100) * (this.years.length - 150));
-        }
-
-        setTimeout(function(){
-            _this.displayedYears = [];
-            for (var i = start; i < (start + 150) && i < _this.years.length; i++) {
-                if (!_this.displayedYears.includes(_this.years[i])) {
-                    _this.items.update({id: _this.displayedYears.length, content: _this.yearDisplay[_this.years[i]][0], start: _this.years[i] + "-01-01"});
-                    _this.displayedYears.push(_this.years[i]);
-                }
-            }
-
-            _this.timeline.fit({animation: true});
-            _this.loadingBlock.style.display = "none";
-        }, 100);
-    }
-    updateSlider(value) { // This function updates the displayed dates after a slider entry
-        var start;
-        if (value == 1) {
-            start = 0;
-        } else {
-            start = Math.floor((value / 100) * (this.years.length - 150));
-        }
-
-        this.hoverInfo1.textContent = this.years[start];
-        this.hoverInfo2.textContent = this.years[start + 149];
-
-        var loc = value * (this.sliderContainer.offsetWidth - 50) / 100;
-    
-        this.hoverInfo1.style.left = (loc - (this.hoverInfo1.offsetWidth + 15)) + "px";
-        this.hoverInfo2.style.left = (loc + 50) + "px";
-    }
+  
 }
-customElements.define('lincs-timeline-horizontal-1', lincsTimelineHoriz1);
-
+export default HybridTimeline;

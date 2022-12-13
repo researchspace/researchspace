47016f5f77b44f4a2689f713571322e0a98c3d8e
Florian Kräutli <florian.kraeutli@uzh.ch>
2022-12-13 10:36:18 +0100

    make accessible from outside docker

--- webpack/webpack.dev.config.js
+++ webpack/webpack.dev.config.js
@@ -28,7 +28,7 @@ module.exports = function() {
 
     config.output.publicPath = 'http://localhost:3000/assets/no_auth/';
     config.devServer = {
-        host: 0.0.0.0,
+        host: "0.0.0.0",
         port: 3000,
         contentBase: './src/main/webapp',
         headers: {

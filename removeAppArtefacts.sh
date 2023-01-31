f53e5427aa4be8e4a10def02c31b000658009a8a
Florian Kräutli <florian.kraeutli@uzh.ch>
2023-01-31 11:02:22 +0100

    Delete removeAppArtefacts.sh

--- removeAppArtefacts.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-APPS=src/main/resources/org/researchspace/apps
-
-mv $APPS/default/data/templates/http%3A%2F%2Fwww.researchspace.org%2Fresource%2F*.html .discard/
-mv $APPS/default/data/templates/Template%3Ahttp%3A%2F%2Fwww.researchspace.org%2Fresource%2F*.html .discard/
-mv $APPS/default/ldp/assets/*.* .discard/

847887d08271e929282211811684d919cbdcbabc
Cristina Giancristofaro <c.giancristofaro@gmail.com>
2024-05-29 18:30:30 +0100

    Fixed ui for Open in new draggable tab
    Signed-off-by: Cristina Giancristofaro <c.giancristofaro@gmail.com>

--- src/main/web/components/forms/inputs/SelectInput.tsx
+++ src/main/web/components/forms/inputs/SelectInput.tsx
@@ -375,7 +375,7 @@ export class SelectInput extends AtomicValueInput<SelectInputProps, State> {
               urlqueryparam-open-as-drag-and-drop="true"
               urlqueryparam-resource={(this.props.value.value as Rdf.Iri).value}
             >
-              <Button className={`${SELECT_TEXT_CLASS}__open-in-new-tab`} title='Open in new tab'>
+              <Button className={`${SELECT_TEXT_CLASS}__open-in-new-tab`} title='Open in new draggable tab'>
                 <Icon iconType='round' iconName='open_in_new' />
               </Button>
           </ResourceLinkContainer>

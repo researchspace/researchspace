4881909bbc4bdcdbfb575206c29f8ea00dfcb64b
Diana Tanase <diana@kartography.org>
2024-10-01 12:01:42 +0100

    Create a share link component and embed it in templates
    Signed-off-by: Diana Tanase <diana@kartography.org>

--- src/main/web/components/copy-to-clipboard/ShareLinkComponent.tsx
+++ src/main/web/components/copy-to-clipboard/ShareLinkComponent.tsx
@@ -20,48 +20,60 @@ import * as React from 'react';
 import * as CopyToClipboard from 'react-copy-to-clipboard';
 
 import { addNotification } from 'platform/components/ui/notification';
+import Rdf = require('platform/api/rdf/core/Rdf');
+import { constructUrlForResource, NavigationUtils } from 'platform/api/navigation';
 
 export interface Props {
-  /**
-   * The text that will be copied to clipboard
-   */
-  text: string;
   /**
    * The message that will be displayed in the notification when the text has been copied
    * @default 'The content has been copied!'
    */
   message?: string;
+
+  /* Todo: support using this parameter, mutually exclusive with the relativePath one */
+  absolutePath?: string;
+
+  relativePath: string;
 }
 
 /**
  * @example
- * <mp-copy-to-clipboard text='text'>
- *     <button class='btn btn-default'>
- *         <rs-icon icon-type="rounded" icon-name="content_copy" symbol="true"></rs-icon>
- *     </button>
- * </mp-copy-to-clipboard>
+ * <rs-share-link relative-path='/resource/ThinkingFrames' 
+ *                urlqueryparam-resource="http://www.researchspace.org/resource/place/c26cd35e-ec3c-4069-b779-47d941f56edc" 
+ *                urlqueryparam-view="resource-editor" urlqueryparam-custom-label="My Place" message='Share link has been copied'>
+ *       <button class="btn btn-default btn-textAndIcon" title="Copy IRI">
+ *       <rs-icon icon-type="rounded" icon-name="content_copy" symbol="true"></rs-icon>
+ *           Share Link to Resource View
+ *       </button>
+ * </rs-share-link>
  */
-export class CopyToClipboardComponent extends React.Component<Props, {}> {
+export class ShareLinkComponent extends React.Component<Props, {}> {
   static defaultProps = {
     message: 'The content has been copied!',
     isUrl: false
   };
 
   private onCopy = () => {
+    
     addNotification({
       level: 'success',
       message: this.props.message,
     });
   };
 
-  render() {  
+  private createQueryString = (obj) => {
+    const params = new URLSearchParams(obj);
+    return params.toString();
+  };
+
+  render() {
+    const params = NavigationUtils.extractParams(this.props);
     return (
-      <CopyToClipboard text={this.props.text} onCopy={this.onCopy}>
+      <CopyToClipboard text={window.location.protocol+"//"+window.location.host+this.props.relativePath+"?"+this.createQueryString(params)} onCopy={this.onCopy}>
         {this.props.children}
       </CopyToClipboard>
-    );
-   
+    );   
   }
 }
 
-export default CopyToClipboardComponent;
+export default ShareLinkComponent;

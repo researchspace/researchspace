ef5f5519924f940e7221e8efe890fe9d8036c034
Diana Tanase <diana@kartography.org>
2026-06-02 20:57:46 +0100

    Add semantic mode for visual item, mark and inscription
    Signed-off-by: Diana Tanase <diana@kartography.org>

--- src/main/web/data/iiif/ImageRegionSchema.ts
+++ src/main/web/data/iiif/ImageRegionSchema.ts
@@ -89,17 +89,82 @@ export const ImageRegionIsPrimaryAreaOf = Forms.normalizeFieldDefinition({
 export const ImageRegionRepresentsVisualItem = Forms.normalizeFieldDefinition({
   id: 'representsVisualItem',
   xsdDatatype: vocabularies.xsd.anyURI,
+  range: '[ "http://www.cidoc-crm.org/cidoc-crm/E26_Physical_Feature", "http://www.cidoc-crm.org/cidoc-crm/E36_Visual_Item", "http://www.cidoc-crm.org/cidoc-crm/E37_Mark", "http://www.cidoc-crm.org/cidoc-crm/E34_Inscription"]',
   insertPattern: `INSERT {
+    $subject <http://www.cidoc-crm.org/cidoc-crm/P138_represents> ?physicalFeature .
+    ?physicalFeature <http://www.cidoc-crm.org/cidoc-crm/P138i_has_representation> $subject .
+
+    $subject <http://www.researchspace.org/ontology/PX_main_represents> ?physicalFeature .
+    ?physicalFeature <http://www.researchspace.org/ontology/PX_has_main_representation> $subject .
+
     $subject <http://www.cidoc-crm.org/cidoc-crm/P138_represents> ?visualItem .
     ?visualItem <http://www.cidoc-crm.org/cidoc-crm/P138i_has_representation> $subject .
+
+    $subject <http://www.researchspace.org/ontology/PX_main_represents> ?visualItem .
+    ?visualItem <http://www.researchspace.org/ontology/PX_has_main_representation> $subject .
+
+
+    ?visualItem a ?rdfType .
+    ?visualItem crm:P2_has_type ?p2Type .
+
     ?visualItem crm:P1_is_identified_by ?visualItemAppellation . 
     ?visualItemAppellation a crm:E41_Appellation . 
     ?visualItemAppellation crm:P2_has_type <http://www.researchspace.org/resource/system/vocab/resource_type/primary_appellation> . 
     ?visualItemAppellation crm:P190_has_symbolic_content ?visualItemLabel .
+
+    # if an object is specified create a physical feature and connect the visual item to it
+    ?objectIri crm:P56_bears_feature ?physicalFeature .
+    ?physicalFeature crm:P56i_is_found_on ?objectIri .
+
+    ?objectIri crm:P65_shows_visual_item ?visualItem .
+    ?visualItem crm:P65i_is_shown_by ?objectIri .
+
+    ?pc130_shows_features_of a crm:PC130_shows_features_of .
+    ?physicalFeature crm:P02i_is_range_of ?pc130_shows_features_of .
+    ?pc130_shows_features_of crm:P02_has_range ?physicalFeature .
+
+    ?pc130_shows_features_of crm:P01_has_domain ?visualItem .
+    ?visualItem crm:P01i_is_domain_of ?pc130_shows_features_of .
+
+    ?physicalFeature a crm:E26_Physical_Feature .
+    ?physicalFeature crm:P1_is_identified_by ?physicalFeatureAppellation . 
+    ?physicalFeatureAppellation a crm:E41_Appellation . 
+    ?physicalFeatureAppellation crm:P2_has_type <http://www.researchspace.org/resource/system/vocab/resource_type/primary_appellation> . 
+    ?physicalFeatureAppellation crm:P190_has_symbolic_content ?physicalFeatureLabel .
+
   } WHERE { 
-      BIND(IRI(CONCAT(STR($subject),"/visual_item/",STRUUID())) as ?visualItem)  
-      BIND(IRI(CONCAT(STR(?visual_item),"/primary_appellation",STRUUID())) as ?visualItemAppellation)
-      BIND("Visual Item" as ?visualItemLabel)
+      BIND(STR(?value) AS ?encoded)
+      
+      FILTER(CONTAINS(?encoded, "&rdfType="))
+      BIND(IRI(REPLACE(?encoded, "^.*&rdfType=([^&]*)&.*$", "$1")) AS ?rdfType)
+
+      OPTIONAL {
+        FILTER(CONTAINS(?encoded, "&p2Type="))
+        BIND(IRI(REPLACE(?encoded, "^.*&p2Type=([^&]*)&.*$", "$1")) AS ?p2Type)
+      }
+
+      OPTIONAL {
+        FILTER(CONTAINS(?encoded, "&objectIri="))
+        BIND(IRI(REPLACE(?encoded, "^.*&objectIri=([^&]*)&.*$", "$1")) AS ?objectIri)
+        # specify the physical feature and connection to the object
+        BIND(IRI(CONCAT(STR(?objectIri), "/physical_feature/", STRUUID())) AS ?physicalFeature)
+        BIND(IRI(CONCAT(STR(?physicalFeature), "/primary_appellation")) AS ?physicalFeatureAppellation)
+        BIND(IRI(CONCAT(STR(?physicalFeature), "/PC130_shows_features_of/",STRUUID())) AS ?pc130_shows_features_of)     
+      }
+
+      FILTER(CONTAINS(?encoded, "&annotationLabel="))
+      BIND(REPLACE(?encoded, "^.*&annotationLabel=([^&]*)&.*$", "$1") AS ?encodedAnnotationLabel)
+  
+      BIND(REPLACE(STR(?encodedAnnotationLabel), "%20", " ") AS ?annotationLabel)
+      BIND(CONCAT("Physical Feature: ",?annotationLabel) as ?physicalFeatureLabel)
+     
+      OPTIONAL {
+        FILTER(!CONTAINS(?encoded,"E26_Physical_Feature"))
+        BIND(IRI(CONCAT(STR($subject),"/visual_item/",STRUUID())) as ?visualItem)  
+      }
+      
+      BIND(IRI(CONCAT(STR(?visualItem),"/primary_appellation",STRUUID())) as ?visualItemAppellation)
+      BIND(CONCAT(STRAFTER(STR(?rdfType),"_"),": ",?annotationLabel) as ?visualItemLabel)
     }`,
   selectPattern: `SELECT ?value WHERE {
     $subject <http://www.cidoc-crm.org/cidoc-crm/P138_represents> ?value .
@@ -257,6 +322,110 @@ export const ImageRegionRepresentsXRFMeasurement = Forms.normalizeFieldDefinitio
   }`,
 });
 
+export const ImageRegionRepresentsDigitalMeasurement = Forms.normalizeFieldDefinition({
+  id: 'representsDigitalMeasurement',
+  xsdDatatype: vocabularies.xsd.anyURI,
+  range: '[ "http://www.cidoc-crm.org/extensions/crmdig/D11_Digital_Measurement_Event", "http://www.researchspace.org/resource/system/vocab/resource_type/measurement_xrf"]',
+  insertPattern: `INSERT {
+    $subject <http://www.cidoc-crm.org/cidoc-crm/P138_represents> ?measurement .
+    ?measurement <http://www.cidoc-crm.org/cidoc-crm/P138i_has_representation> $subject .
+    $subject <http://www.researchspace.org/ontology/PX_main_represents> ?measurement .
+    ?measurement <http://www.researchspace.org/ontology/PX_has_main_representation> $subject .
+
+    ?measurement crm:P9i_forms_part_of ?examinationIri . 
+    ?examinationIri crm:P9_consists_of ?measurement . 
+
+    # Measurement on Full Object
+    ?measurement crmsci:O24_measured ?fullObjectIri .
+    ?fullObjectIri crmsci:O24i_was_measured_by ?measurement .
+
+    # Measurement on Physical Feature
+    ?measurement crmsci:O24_measured ?physicalFeature .
+    ?physicalFeature crmsci:O24i_was_measured_by ?measurement .
+
+    ?physicalFeature a crm:E26_Physical_Feature .
+    ?physicalFeature crm:P2_has_type <http://www.researchspace.org/resource/system/vocab/resource_type/measurement_site> .
+    ?physicalFeature crm:P56i_is_found_on ?objectIri .
+    ?objectIri crm:P56_bears_feature ?physicalFeature .
+
+    $subject <http://www.cidoc-crm.org/cidoc-crm/P138_represents> ?physicalFeature .
+    ?physicalFeature <http://www.cidoc-crm.org/cidoc-crm/P138i_has_representation> $subject .
+    $subject <http://www.researchspace.org/ontology/PX_main_represents> ?physicalFeature .
+    ?physicalFeature <http://www.researchspace.org/ontology/PX_has_main_representation> $subject .
+
+
+    ?physicalFeature crm:P1_is_identified_by ?physicalFeatureAppellation .
+    ?physicalFeatureAppellation a crm:E41_Appellation .
+    ?physicalFeatureAppellation crm:P2_has_type <http://www.researchspace.org/resource/system/vocab/resource_type/primary_appellation> . 
+    ?physicalFeatureAppellation crm:P190_has_symbolic_content ?physicalFeatureLabel .
+    
+    ?measurement a ?rdfType .
+    ?measurement crm:P2_has_type ?p2Type .
+
+    ?measurement crm:P1_is_identified_by ?measurementAppellation . 
+    ?measurementAppellation a crm:E41_Appellation . 
+    ?measurementAppellation crm:P2_has_type <http://www.researchspace.org/resource/system/vocab/resource_type/primary_appellation> . 
+    ?measurementAppellation crm:P190_has_symbolic_content ?measurementLabel .
+  } WHERE {
+      BIND(STR(?value) AS ?encoded)
+      
+      FILTER(CONTAINS(?encoded, "&rdfType="))
+      BIND(IRI(REPLACE(?encoded, "^.*&rdfType=([^&]*)&.*$", "$1")) AS ?rdfType)
+
+      OPTIONAL {
+        FILTER(CONTAINS(?encoded, "&p2Type="))
+        BIND(IRI(REPLACE(?encoded, "^.*&p2Type=([^&]*)&.*$", "$1")) AS ?p2Type)
+      }
+      
+      OPTIONAL {
+        FILTER(CONTAINS(?encoded, "&examinationIri="))
+        BIND(IRI(REPLACE(?encoded, "^.*&examinationIri=([^&]*)&.*$", "$1")) AS ?examinationIri)
+      }
+
+      OPTIONAL {
+        FILTER(CONTAINS(?encoded, "&objectIri="))
+        BIND(IRI(REPLACE(?encoded, "^.*&objectIri=([^&]*)&.*$", "$1")) AS ?objectIri)
+      }
+
+      OPTIONAL {
+        FILTER(CONTAINS(?encoded, "&measurementOnFullObject="))
+        BIND(REPLACE(?encoded, "^.*&measurementOnFullObject=([^&]*)&.*$", "$1") AS ?measurementOnFullObject)
+        FILTER(CONTAINS(?encoded, "&objectIri="))
+        BIND(IRI(REPLACE(?encoded, "^.*&objectIri=([^&]*)&.*$", "$1")) AS ?fullObjectIri)  
+      }
+
+      OPTIONAL {
+        FILTER(CONTAINS(?encoded, "&measurementOnObjectPhysicalFeature="))
+        BIND(REPLACE(?encoded, "^.*&measurementOnObjectPhysicalFeature=([^&]*)&.*$", "$1") AS ?measurementOnObjectPhysicalFeature)
+        FILTER(CONTAINS(?encoded, "&objectIri="))
+        BIND(IRI(REPLACE(?encoded, "^.*&objectIri=([^&]*)&.*$", "$1")) AS ?objectIri)
+        BIND(IRI(CONCAT(STR(?objectIri), "/physical_feature/", STRUUID())) AS ?physicalFeature)
+        BIND(IRI(CONCAT(STR(?physicalFeature), "/primary_appellation")) AS ?physicalFeatureAppellation)
+      }
+
+      OPTIONAL {
+        FILTER(CONTAINS(?encoded, "&measurementOnObjectSample="))
+        BIND(REPLACE(?encoded, "^.*&measurementOnObjectSample=([^&]*)&.*$", "$1") AS ?measurementOnObjectSample)
+      }
+
+      FILTER(CONTAINS(?encoded, "&annotationLabel="))
+      BIND(REPLACE(?encoded, "^.*&annotationLabel=([^&]*)&.*$", "$1") AS ?encodedAnnotationLabel)
+  
+      BIND(REPLACE(STR(?encodedAnnotationLabel), "%20", " ") AS ?annotationLabel)
+      BIND(CONCAT("Physical Feature: ",?annotationLabel) as ?physicalFeatureLabel)
+      BIND(STRBEFORE(?encoded,"&annotationLabel=") as ?iriStr)
+
+      BIND(IRI(?iriStr) AS ?annotationIri)     
+      BIND(IRI(CONCAT(STR(?examinationIri),"/measurement/",STRUUID())) as ?measurement)           
+  	  BIND(URI(CONCAT(STR(?measurement), "/primary_appellation") ) as ?measurementAppellation)
+      BIND(CONCAT("Digital Measurement: ",?annotationLabel) as ?measurementLabel)
+    }`,
+  selectPattern: `SELECT ?value WHERE {
+    $subject <http://www.cidoc-crm.org/cidoc-crm/P138_represents> ?value .
+    
+  }`,
+});
+
 // TODO
 // Add representation for Measurement, Mark, Visual Item, and Feature and what else Person, etc.
 // How do we model this can we just model it generically and say represents and if one of these other semantic annotation modes it uses the ImageRegionRepresentsEntity?!
@@ -294,6 +463,6 @@ export const ImageRegionFields: ReadonlyArray<Forms.FieldDefinition> = [
   ImageRegionViewport,
   ImageRegionIsPrimaryAreaOf,
   ImageRegionRepresentsSamplingSite,
-  ImageRegionRepresentsXRFMeasurement
-  //ImageRegionRepresentsVisualItem
+  ImageRegionRepresentsDigitalMeasurement,
+  ImageRegionRepresentsVisualItem
 ];

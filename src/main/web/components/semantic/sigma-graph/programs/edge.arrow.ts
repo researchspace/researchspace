cb7991ecec1fe96b0c7f0219393a55126a62447d
Diana Tanase <diana@kartography.org>
2026-06-22 05:10:59 +0100

    Add contributors license
    Signed-off-by: Diana Tanase <diana@kartography.org>

--- src/main/web/components/semantic/sigma-graph/programs/edge.arrow.ts
+++ src/main/web/components/semantic/sigma-graph/programs/edge.arrow.ts
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) 2026 ResearchSpace contributors.
+ * 
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
 import { createEdgeCompoundProgram } from "sigma/rendering/webgl/programs/common/edge";
 import EdgeArrowHeadProgram from "./edge.arrowHead";
 import EdgeClampedProgram from "sigma/rendering/webgl/programs/edge.clamped";
--- src/main/web/components/semantic/sigma-graph/programs/edge.arrowHead.ts
+++ src/main/web/components/semantic/sigma-graph/programs/edge.arrowHead.ts
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) 2026 ResearchSpace contributors.
+ * 
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
 import { floatColor } from "sigma/utils";
 import vertexShaderSource from "sigma/rendering/webgl/shaders/edge.arrowHead.vert.glsl";
 import fragmentShaderSource from "sigma/rendering/webgl/shaders/edge.arrowHead.frag.glsl";

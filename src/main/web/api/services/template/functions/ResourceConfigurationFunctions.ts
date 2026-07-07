020c18596b511a394924fd7e9e90c26a755c9758
Diana Tanase <diana@kartography.org>
2026-07-07 20:17:43 +0100

    Switch back to default repo for getResourceConfiguration
    Signed-off-by: Diana Tanase <diana@kartography.org>

--- src/main/web/api/services/template/functions/ResourceConfigurationFunctions.ts
+++ src/main/web/api/services/template/functions/ResourceConfigurationFunctions.ts
@@ -9,6 +9,6 @@ export const ResourceConfigurationFunctions = {
   },
 
   getResourceConfiguration: function(iri:string) {       
-    return getResourceConfiguration(Rdf.iri(iri),"fedx");
+    return getResourceConfiguration(Rdf.iri(iri),"default");
   }
 };
